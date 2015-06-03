2e28d12e27964fb4a08ea0c8e8e37cd485cb3288
cherry <fubei1989@163.com>
2015-06-03 14:54:39 +0800

    day3

--- awesome-python3-webapp/www/orm.py
+++ awesome-python3-webapp/www/orm.py
@@ -1,7 +1,14 @@
-#!usr/local/bin/python3
+#!/usr/local/bin/python3
+#_*_ coding: utf-8 _*_
 
-from orm import Model, StringField, IntegerField
+__author__ = 'qunny'
 
+import asyncio, logging
+
+import aiomysql
+
+def log(sql, args=()):
+	logging.info('SQL:%s' % sql)
 
 @asyncio.coroutine
 def create_pool(loop, **kw):
@@ -19,8 +26,6 @@ def create_pool(loop, **kw):
 		minsize = kw.get('minsize', 1),
 		loop = loop
 	)
-
-
 
 @asyncio.coroutine
 def select(sql, args, size=None):
@@ -53,6 +58,88 @@ def execute(sql, args):
 		return affected
 
 
+def create_args_string(num):
+	L = []
+	for n in range(num):
+		L.append('?')
+	return ', '.join(L)
+
+class Field(object):
+	def __init__(self, name, column_type, primary_key, default):
+		self.name = name
+		self.column_type = column_type
+		self.primary_key = primary_key
+		self.default = default
+
+	def __str__(self):
+		return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
+
+class StringField(Field):
+	def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+		super().__init__(name, ddl, primary_key, default)
+
+
+class BooleanField(Field):
+	def __init__(self, name=None, default=False):
+		super().__init__(name, 'boolean', False, default)
+
+class IntegerField(Field):
+	def __init__(self, name=None, primary_key=False, default=0):
+		super().__init__(name, 'bigint', primary_key, default)
+
+class FloatField(Field):
+	def __init__(self, name=None, primary_key=False, default=0.0):
+		super().__init__(name, 'real', primary_key, default)
+
+class TextField(Field):
+	def __init__(self, name=None, default=None):
+		super().__init__(name, 'text', False, default)
+
+
+class ModelMetaclass(type):
+
+	def __new__(cls, name, bases, attrs):
+		#排除Model类本身
+		if name=='Model':
+			return type.__new__(cls, name, bases, attrs)
+		#获取table名称
+		tableName = attrs.get('__table__', None) or name
+		logging.info('found model: %s (table: %s)' % (name, tableName))
+		#获取所有的Field和主键名
+		mappings = dict()
+		fields = []
+		primaryKey = None
+		for k, v in attrs.items():
+			if isinstance(v, Field):
+				logging.info(' found mappings: %s ==> %s' % (k, v))
+				mappings[k] = v
+				if v.primary_key:
+					if primaryKey:
+						raise StandardError('Duplicate primary key for filed: %s' % k)
+					primaryKey = k
+				else:
+					fields.append(k)
+
+		if not primaryKey:
+			raise StandardError('primary key not found')
+
+		for k in mappings.keys():
+			attrs.pop(k)
+
+		escaped_fields = list(map(lambda f: '`%s`' % f, fields))
+		attrs['__mappings__'] = mappings  #保存属性和列的映射关系
+		attrs['__table__'] = tableName
+		attrs['__primary_key'] = primaryKey
+		attrs['__fields__'] = fields
+
+		attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
+		attrs['__insert__'] = 'insert into `%s` (%s, `%s`) value (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields)+1))
+		attrs['__update__'] = 'update `%s` set %s where `%s` = ?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+		attrs['__delete__'] = 'delete from `%s` where `%s` = ?' % (tableName, primaryKey)
+
+		return type.__new__(cls, name ,bases, attrs)
+
+
 # 定义所有ORM映射的基类Model
 class Model(dict, metaclass=ModelMetaclass):
 
@@ -81,12 +168,43 @@ class Model(dict, metaclass=ModelMetaclass):
 				setattr(self, key, value)
 		return value
 
+	@classmethod
+	@asyncio.coroutine
+	def findAll(cls, where=None, args=None, **kw):
+		' find objects by where clause. '
+		sql = [cls.__select__]
+		if where:
+			sql.append('where')
+			sql.append(where)
+
+		if args is None:
+			args = []
+
+		orderBy = kw.get('orderBy', None)
+		if orderBy:
+			sql.append('order by ')
+			sql.append(orderBy)
+
+		limit = kw.get('limit', None)
+		if limit is not None:
+			sql.append('limit')
+			if isinstance(limit, int):
+				sql.append('?')
+				args.append(limit)
+			elif isinstance(limit, tuple) and len(limit) == 2:
+				sql.append('?, ?')
+				args.extend(limit)
+			else:
+				raise ValueError('Invalid limit value: %s' % str(limit))
+
+		rs = yield from select(' '.join(sql), args)
+		return [cls(**r) for r in rs]
 
 	@classmethod
 	@asyncio.coroutine
 	def find(cls, pk):
 		' find object by primary key. '
-		rs = yield from select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
+		rs = yield from select('%s where ` %s ` = ?' % (cls.__select__, cls.__primary_key__), [pk], 1)
 		if len(rs) == 0:
 			return None
 		return cls(**rs[0])
@@ -99,63 +217,20 @@ class Model(dict, metaclass=ModelMetaclass):
 		if row != 1:
 			logging.warn('failed to insert record: affected rows: %s ' % rows)
 
+	@asyncio.coroutine
+	def update(self):
+		args = list(map(self.getValue, self.__fields__))
+		args.append(self.getValue(self.__primary_key__))
+		rows = yield from execute(self.__update__, args)
+		if row != 1:
+			logging.warn('failed to update by primary key: affected rows: %s' % rows)
 
-class Field(object):
-	def __init__(self, name, column_type, primary_key, default):
-		self.name = name
-		self.column_type = column_type
-		self.primary_key = primary_key
-		self.default = default
-
-	def __str__(self):
-		return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
-
-class StringField(Field):
-	def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
-		super().__init__(name, ddl, primary_key, default)
-
-
-class ModelMetaclass(type):
-	def __new__(cls, name, bases, attrs):
-		#排除Model类本身
-		if name=='Model':
-			return type.__new__(cls, name, bases, attrs)
-		#获取table名称
-		tableName = attrs.get('__table__', None) or name
-		logging.info('found model: %s (table: %s' % (name, tableName))
-		#获取所有的Field和主键名
-		mappings = dict()
-		fields = []
-		primaryKey = None
-		for k, v in attrs.items():
-			if isinstance(v, Field):
-				logging.info(' found mapping: %s ==> %s' % (k, v))
-				mappiing[k] = v
-				if v.primary_key:
-					if primaryKey:
-						raise StandardError('Duplicate primary key for filed: %s' % k)
-					primaryKey = k
-				else:
-					fields.append(k)
-
-		if not primaryKey:
-			raise StandardError('primary key not found')
-		for k in mappiing.keys():
-			attrs.pop(k)
-
-		escaped_fields = list(map(lambda f: '`%s`' % f, fields))
-		attrs['__mappings__'] = mapping  #保存属性和列的映射关系
-		attrs['__table__'] = tableName
-		attrs['__primary_key'] = primaryKey
-		attrs['__fields__'] = fields
-
-		attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
-		attrs['__insert__'] = 'insert into `%s` (%s, `%s`) value (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields)+1))
-		attrs['__update__'] = 'update `%s` set %s where `%s` = ?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
-		attrs['__delete__'] = 'delete from `%s` where `%s` = ?' % (tableName, primaryKey)
-
-		return type.__new__(cls, name ,bases, attrs)
-
+	@asyncio.coroutine
+	def remove(self):
+		args = [self.getValue(self.__primary_key__)]
+		rows = yield from execute(self.__delete__, args)
+		if rows != 1:
+			logging.warn('failed to remove by primary key: affected rows: %s' % rows)		
 
 
 class User(Model):
@@ -164,13 +239,11 @@ class User(Model):
 	id = IntegerField(primary_key=True)
 	name = StringField()
 
-
-
 user = User(id=123, name='Michael')
 # save to db
 user.insert()
 # select 
-users = User.findAll()
+# users = User.findAll()
 
 
 
--- awesome-python3-webapp/www/static/app.py
+++ awesome-python3-webapp/www/static/app.py
@@ -1,20 +1,29 @@
 #!/usr/local/bin/python3
+# _*_ coding:utf-8 -*-
 
+__author__ = 'qunny'
+
+...
+async web application
+...
 
 import logging; logging.basicConfig(level=logging.INFO)
 
 import asyncio, os, json, time
-from datetime import datetime
 
+from datetime import datetime
 from aiohttp import web
 
-def index(request):
-	return web.Response(body=b'<h1>Awesome</h1>')
+import orm
+
+# def index(request):
+# 	return web.Response(body=b'<h1>Awesome</h1>')
 
 @asyncio.coroutine
 def init(loop):
+	yield from orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='www', password='www', db='awesome')
 	app = web.Application(loop=loop)
-	app.router.add_route('GET', '/', index)
+	# app.router.add_route('GET', '/', index)
 	src = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
 	logging.info('server started at http://127.0.0.1:9000...')
 	return src
