8f3b6a9d00d6b68522bde5f6eee82b1074c12e74
qunny <fubei1989@163.com>
2015-09-21 22:40:44 +0800

    sync

--- flask-web/thorn/flaskHello.py
+++ flask-web/thorn/flaskHello.py
@@ -1,10 +1,58 @@
-from flask import Flask
+#-*-coding:utf-8-*-
+
+from flask import Flask, url_for
 app = Flask(__name__)
 
-@app.route('/')
+# 开启debug模式，修改文件后会重新加载文件
+app.debug = True
 
-def hello_world():
-	return 'Hello World Flask!'
+########## route begin ##########
+# @app.route('/')
+# def hello_world():
+# 	return 'Thorn Index Page!'
+
+# @app.route('/f')
+# def hello():
+# 	return 'Thorn Front Page!'
+
+# @app.route('/user/<username>')
+# def show_user_profile(username):
+# 	# show the use profile for that user
+# 	return 'welcome User %s' % username
+
+# @app.route('/post/<int:post_id>')
+# def show_post(post_id):
+# 	# show the post with the given id, the id is an integer
+# 	return 'Thorn Post %d' % post_id
+
+# @app.route('/project/')
+# def projects():
+# 	return 'The project page'
+
+# @app.route('/about')
+# def about():
+# 	return 'Thorn about page'
+########## route end ##########
+
+########## URL Contruct ##########
+@app.route('/')
+def index():
+	pass
+
+@app.route('/login')
+def login():
+	pass
+
+@app.route('/user/<username>')
+def profile(username):
+	pass
+
+with app.test_request_context():
+	print url_for('index')
+	print url_for('login')
+	print url_for('login', next = '/')
+	print url_for('profile', username='john Doe')
+
 
 if __name__ == '__main__':
-	app.run()
+	app.run(host='192.168.1.5')
